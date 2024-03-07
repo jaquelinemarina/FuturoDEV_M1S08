6d872e924f304d3bdd3690c1ab9e492cfcc11961
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-03-07 19:36:11 -0300

    Ex 2 - Selecionar elemento por classe

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//Ex 1 - Selecionar elemento por ID
 let titulo = document.getElementById('title')
 
 titulo.style.backgroundColor = 'purple'
+
+
+// Ex 2 - Selecionar elemento por classe
+let trocarParagrafo = document.getElementsByClassName('paragrafo')
+
+trocarParagrafo.innerText = "Alterando o conteúdo dos parágrafos utilizando o DOM."
+console.log(trocarParagrafo)
